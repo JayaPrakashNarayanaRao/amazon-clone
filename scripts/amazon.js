91a747a7003b609080fbc5db8467ac23662ac071
JayaPrakashNarayanaRao <ravijayaprakashnarayanarao@gmail.com>
2024-07-24 21:32:39 +0530

    added search functionality

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -7,16 +7,13 @@ loadProducts(renderProductsGrid);
 function renderProductsGrid() {
   let productsHTML = '';
 
-const url = new URL(window.location.href);
-const search = url.searchParams.get('search');
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
 
-let filteredProducts = products;
-
-// If a search exists in the URL parameters,
-// filter the products that match the search.
-if (search) {
-  filteredProducts = products.filter((product) => {
-    let matchingKeyword = false;
+  let filteredProducts = products;
+  if (search) {
+    filteredProducts = products.filter((product) => {
+      let matchingKeyword = false;
 
       product.keywords.forEach((keyword) => {
         if (keyword.toLowerCase().includes(search.toLowerCase())) {
@@ -26,70 +23,76 @@ if (search) {
 
       return matchingKeyword ||
         product.name.toLowerCase().includes(search.toLowerCase());
-  });
-}
+    });
+  }
 
-filteredProducts.forEach((product) => {
-    productsHTML += `
-      <div class="product-container">
-            <div class="product-image-container">
-              <img class="product-image"
-                src="${product.image}">
-            </div>
-
-            <div class="product-name limit-text-to-2-lines">
-              ${product.name} 
-            </div>
-
-            <div class="product-rating-container">
-              <img class="product-rating-stars"
-                src="${product.getStarsrl()}">
-              <div class="product-rating-count link-primary">
-                ${product.rating.count}
-              </div>
-            </div>
-
-            <div class="product-price">
-              ${product.getPrice()}
-            </div>
-
-            <div class="product-quantity-container">
-              <select class="js-quantity-selector-${product.id}">
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
-
-            ${product.extraInfoHTML()}
-
-            <div class="product-spacer"></div>
-
-            <div class="added-to-cart js-added-to-cart-${product.id}">
-              <img src="images/icons/checkmark.png">
-              Added
-            </div>
-
-            <button class="add-to-cart-button button-primary js-add-to-cart"
-            data-product-id="${product.id}">
-              Add to Cart
-            </button>
+  if (filteredProducts.length === 0) {
+    productsHTML = '<div class="no-results-found">Sorry, No Products Found!</div>';
+  } else {
+    filteredProducts.forEach((product) => {
+      productsHTML += `
+        <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
           </div>
-    `;
-  });
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="${product.getStarsrl()}">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            ${product.getPrice()}
+          </div>
+
+          <div class="product-quantity-container">
+            <select class="js-quantity-selector-${product.id}">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          ${product.extraInfoHTML()}
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart js-added-to-cart-${product.id}">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}">
+            Add to Cart
+          </button>
+        </div>
+      `;
+    });
+  }
+
   document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
   const addedMessageTimeouts = {};
 
   function updateCartQuantity(productId){
     let cartQuantity = calculateCartQuantity();
-    
+
     if(cartQuantity === 0){
       document.querySelector('.js-cart-quantity').innerHTML = '';
     }
@@ -129,14 +132,14 @@ filteredProducts.forEach((product) => {
   document.querySelector('.js-search-button')
     .addEventListener('click', () => {
       const search = document.querySelector('.js-search-bar').value;
-      window.location.href = `amazon.html?search=${search}`;
+      window.location.href = `index.html?search=${search}`;
     });
 
   document.querySelector('.js-search-bar')
   .addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
       const searchTerm = document.querySelector('.js-search-bar').value;
-      window.location.href = `amazon.html?search=${searchTerm}`;
+      window.location.href = `index.html?search=${searchTerm}`;
     }
   });
 }
